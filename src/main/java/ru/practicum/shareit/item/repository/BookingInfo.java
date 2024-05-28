544a8942afe7213eb4cc2576701e113e9f024b78
akhmed <44803512+Kumyshev@users.noreply.github.com>
2024-05-28 21:32:01 +0300

    Update BookingInfo.java

--- src/main/java/ru/practicum/shareit/item/repository/BookingInfo.java
+++ src/main/java/ru/practicum/shareit/item/repository/BookingInfo.java
@@ -4,5 +4,6 @@ import ru.practicum.shareit.item.model.Booking;
 
 public interface BookingInfo {
     public Booking findLastBooking(Long userId, Long itemId);
+
     public Booking findNextBooking(Long userId, Long itemId);
 }
